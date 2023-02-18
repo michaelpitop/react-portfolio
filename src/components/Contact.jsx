f98abf4bc597d60b363b6ef43bf442d8fd1af2e3
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2023-02-18 13:00:47 +0100

    added some CSS properties in Contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import "../styles.css";
 
 function Contact() {
       return (
+        <div class="cont-container">
+          <div class="cont-card">
             <h1>this is the contact page</h1>
+          </div>
+          <div class="cont-card">
+            <h2>and this a h2 element</h2>
+          </div>
+        </div>
         );
 }
 
