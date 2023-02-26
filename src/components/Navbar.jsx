64f3cece200ca65e6ed2b028f04d136815e16e03
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2023-02-26 12:17:08 +0100

    managed to get tailwind to work :)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,12 +7,12 @@ import "../styles.css";
 
 function Navbar() {
   return (
-    <nav className="flex justify-between items-center text-white">
+    <nav className="flex justify-between items-right text-white text-3xl font-bold underline">
       <div class="container">
-        <div class="column"><Link to="/">Home</Link></div>
-        <div class="column"><Link to="./project">Projects</Link></div>
-        <div class="column"><Link to="./contact">Contact</Link></div>
-        <div class="column"><Link to="./certs">Certificates</Link></div>
+          <div class="column"><Link to="/"><h3>Home</h3></Link></div>
+          <div class="column"><Link to="./project"><h3>Projects</h3></Link></div>
+          <div class="column"><Link to="./contact"><h3>Contact</h3></Link></div>
+          <div class="column"><Link to="./certs"><h3>Certificates</h3></Link></div>
       </div>
     </nav>
   );
