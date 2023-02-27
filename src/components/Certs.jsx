ca02d69e126ee9817b768043bc5e00f9df541fe0
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2023-02-27 14:45:46 +0100

    added pictures in the certificates page

--- src/components/Certs.jsx
+++ src/components/Certs.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import "../styles.css";
+import rwd from "../assets/certs/rwd.png";
+import jaads from "../assets/certs/jaads.png";
+import fedl from "../assets/certs/fedl.png";
+import diploma from "../assets/certs/diploma.png"
 
 function Certs() {
   return (
-    <div class="container row">
-      <div class="col-4"><a href="https://www.freecodecamp.org/certification/michaelpitop/responsive-web-design">Responsive Web Desing</a></div>
-      <div class="col-4"><a href="https://freecodecamp.org/certification/michaelpitop/front-end-development-libraries">Front End Development Libraries</a></div>
-      <div class="col-4"><a href="https://www.free  codecamp.org/certification/michaelpitop/javascript-algorithms-and-data-structures">JavaScript Algorithms and Data Structures</a></div>
-      <div class="col-4"><a href="https://kitt.lewagon.com/schoolings/15683/public_diploma?token=ba7a269adc951a523107c55ef0338f1a4eee4d4e59173ab30cf5010d051f7fcf">Le Wagon Munich Full stack web development bootcamp</a></div>
+    <div class="grid grid-cols-4 gap-4">
+      <div ><a href="https://www.freecodecamp.org/certification/michaelpitop/responsive-web-design"><img src={rwd} alt='Responsive Web Design' /></a></div>
+      <div ><a href="https://freecodecamp.org/certification/michaelpitop/front-end-development-libraries"><img src={fedl} alt="Front End Development Libraries" /> </a></div>
+      <div ><a href="https://www.freecodecamp.org/certification/michaelpitop/javascript-algorithms-and-data-structures"><img src={jaads} alt="JavaScript Algorithms and Data Structures" /> </a></div>
+      <div ><a href="https://kitt.lewagon.com/schoolings/15683/public_diploma?token=ba7a269adc951a523107c55ef0338f1a4eee4d4e59173ab30cf5010d051f7fcf"> <img src={diploma} alt="Le Wagon Munich Full stack web development bootcamp" /> </a></div>
     </div>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,13 +5,11 @@ function Footer() {
   return (
     <div className="mt-4 bg-dark-200 rounded-md text-white px-8 py-4">
       <ul className="text-center">
-        <li><a href="#home" className="hover:underline">About</a></li>
+        <li><a href="#home" className="hover:underline">About</a></li> {/*need to work the links in the future */}
         <li><a href="#skills" className="hover:underline">Skills</a></li>
+        <li><a href="/project" className="hover:underline">Projects</a></li>
         <li><a href="#certs" className="hover:underline">Certifications</a></li>
       </ul>
-
-      {/* <img src={} className="mb-4 mt-4 w-full md:h-1 text-white bottom-0" alt="hr" /> */}
-
     </div>
   )
 }
