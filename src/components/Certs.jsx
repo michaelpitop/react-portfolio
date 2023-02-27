4ef819870ac220833f32dcd5cd54587dded9dbd7
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2023-02-27 13:25:30 +0100

    added pictures of certificates

--- src/components/Certs.jsx
+++ src/components/Certs.jsx
@@ -6,7 +6,7 @@ function Certs() {
     <div class="container row">
       <div class="col-4"><a href="https://www.freecodecamp.org/certification/michaelpitop/responsive-web-design">Responsive Web Desing</a></div>
       <div class="col-4"><a href="https://freecodecamp.org/certification/michaelpitop/front-end-development-libraries">Front End Development Libraries</a></div>
-      <div class="col-4"><a href="https://www.freecodecamp.org/certification/michaelpitop/javascript-algorithms-and-data-structures">JavaScript Algorithms and Data Structures</a></div>
+      <div class="col-4"><a href="https://www.free  codecamp.org/certification/michaelpitop/javascript-algorithms-and-data-structures">JavaScript Algorithms and Data Structures</a></div>
       <div class="col-4"><a href="https://kitt.lewagon.com/schoolings/15683/public_diploma?token=ba7a269adc951a523107c55ef0338f1a4eee4d4e59173ab30cf5010d051f7fcf">Le Wagon Munich Full stack web development bootcamp</a></div>
     </div>
   );
