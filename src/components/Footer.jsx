9ca1631dbf486c5a55c41a6e51130a21f1ce545a
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2023-03-11 13:43:05 +0100

    added a new picture for main text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,14 @@ import "../styles.css";
 
 function Footer() {
   return (
-    <div class="flex gap-3 justify-center mt-4 bg-dark-200 rounded-md text-white px-8 py-4">
+    <footer class="flex relative bottom-0 left-0 right-0 gap-3 justify-center mt-4 bg-dark-200 rounded-md text-white px-8 py-4">
       <p><a href="/" class="hover:underline">About</a></p> {/*need to work the links in the future */}
       <p><a href="#skill" class="hover:underline">Skills</a></p>
       <p><a href="/project" class="hover:underline">Projects</a></p>
       <p><a href="/certs" class="hover:underline">Certifications</a></p>
       |
       <p>created by michaelpitop</p>
-    </div>
+    </footer>
   )
 }
 
--- src/components/Text1.jsx
+++ src/components/Text1.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import "../styles.css";
-import pic from "../assets/certs/pic.jpg"
+import pic1 from "../assets/pictures/gaidouronisi.jpg"
 
 function Text1() {
 
   return (
-    <div class="flex">
-      <div class="columns-xs">
-        <img src={pic} alt="me!" /></div>
+    <div>
+      <div>
+        <img src={pic1} alt="" /></div>
       <div class="justify-center">
         <h1>Hello! My name is Michael Pitopoulakis</h1>
-        <h2>I'm a Front-end developer </h2>
+        <h2>I'm a Junior Fullstack developer </h2>
       </div>
     </div>
   );
