4163326c3da520b7d6f34d611c6ecdb4c2280210
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2023-03-12 13:41:25 +0100

    minor changes to background positioning

--- src/components/Text1.jsx
+++ src/components/Text1.jsx
@@ -1,15 +1,13 @@
 import React from "react";
-// import "../styles.css";
+import "../styles.css";
 // import pic1 from "../assets/pictures/gaidouronisi.jpg"
 
 function Text1() {
 
   return (
-    <div class="h-screen grid	place-items-center text-center text-white bg-cover bg-no-repeat bg-center bg-[url('/home/michaelpitop/code/michaelpitop/react-portfolio/src/assets/pictures/gaidouronisi.jpg')]">
+    <div class="relative w-screen -inset-x-40	 h-screen grid	place-items-center text-center text-white bg-cover bg-no-repeat bg-center bg-[url('/home/michaelpitop/code/michaelpitop/react-portfolio/src/assets/pictures/leuka_ori.jpg')]">
         <h1>Hello! My name is Michael Pitopoulakis
           <br /> I'm a Junior Fullstack developer</h1>
-      {/* <div class="grid justify-evenly	 text-white">
-      </div> */}
     </div>
   );
 }
