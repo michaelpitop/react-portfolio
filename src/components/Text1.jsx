2e10e0750df7ac1e4f328d3d2b520c31a2a76708
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2023-03-12 12:02:11 +0100

    added a background image in the main page

--- src/components/Text1.jsx
+++ src/components/Text1.jsx
@@ -5,7 +5,7 @@ import React from "react";
 function Text1() {
 
   return (
-    <div class="bg-cover bg-no-repeat bg-center bg-[url('/home/michaelpitop/code/michaelpitop/react-portfolio/src/assets/pictures/gaidouronisi.jpg')]">
+    <div class="h-screen bg-cover bg-no-repeat bg-center bg-[url('/home/michaelpitop/code/michaelpitop/react-portfolio/src/assets/pictures/gaidouronisi.jpg')]">
       <div class="flex justify-center text-white">
         <h1>Hello! My name is Michael Pitopoulakis</h1>
         <h2>I'm a Junior Fullstack developer </h2>
