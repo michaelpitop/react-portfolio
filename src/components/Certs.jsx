deb36ab6782640805a3bfbf556967fe63f0d0543
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2023-03-17 13:33:05 +0100

    added transitions to certs to make them seem clickable

--- src/components/Certs.jsx
+++ src/components/Certs.jsx
@@ -12,10 +12,10 @@ function Certs() {
         <p> <strong><em>Certificates</em></strong></p>
       </div>
       <div className="grid grid-cols-4 gap-4">
-        <div className="box-border h-132 w-132 p-4"><a href="https://www.freecodecamp.org/certification/michaelpitop/responsive-web-design"><img src={rwd} alt='Responsive Web Design' /></a></div>
-        <div className="box-border h-132 w-132 p-4"><a href="https://freecodecamp.org/certification/michaelpitop/front-end-development-libraries"><img src={fedl} alt="Front End Development Libraries" /> </a></div>
-        <div className="box-border h-132 w-132 p-4"><a href="https://www.freecodecamp.org/certification/michaelpitop/javascript-algorithms-and-data-structures"><img src={jaads} alt="JavaScript Algorithms and Data Structures" /> </a></div>
-        <div className="box-border h-132 w-132 p-4" ><a href="https://kitt.lewagon.com/schoolings/15683/public_diploma?token=ba7a269adc951a523107c55ef0338f1a4eee4d4e59173ab30cf5010d051f7fcf"> <img  src={diploma} alt="Le Wagon Munich Full stack web development bootcamp" /> </a></div>
+        <div className="box-border h-132 w-132 p-4 transition duration-300 ease-in-out hover:scale-110"><a href="https://www.freecodecamp.org/certification/michaelpitop/responsive-web-design"><img src={rwd} alt='Responsive Web Design' /></a></div>
+        <div className="box-border h-132 w-132 p-4 transition duration-300 ease-in-out hover:scale-110"><a href="https://freecodecamp.org/certification/michaelpitop/front-end-development-libraries"><img src={fedl} alt="Front End Development Libraries" /> </a></div>
+        <div className="box-border h-132 w-132 p-4 transition duration-300 ease-in-out hover:scale-110"><a href="https://www.freecodecamp.org/certification/michaelpitop/javascript-algorithms-and-data-structures"><img src={jaads} alt="JavaScript Algorithms and Data Structures" /> </a></div>
+        <div className="box-border h-132 w-132 p-4 transition duration-300 ease-in-out hover:scale-110" ><a href="https://kitt.lewagon.com/schoolings/15683/public_diploma?token=ba7a269adc951a523107c55ef0338f1a4eee4d4e59173ab30cf5010d051f7fcf"> <img  src={diploma} alt="Le Wagon Munich Full stack web development bootcamp" /> </a></div>
       </div>
     </div>
   );
