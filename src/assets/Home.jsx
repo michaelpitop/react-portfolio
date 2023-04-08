a37e340d71f1a4cbb175d4fbad2db3f8a09b57f6
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2023-04-09 01:35:31 +0200

    minor fixes and changes to some text

--- src/assets/Home.jsx
+++ src/assets/Home.jsx
@@ -19,7 +19,7 @@ function Home() {
           <h2 class="my-4">Junior Fullstack Developer | Junior Frontend Developer</h2>
           <div class="flex  place-content-center	">
             <button type="button" class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-3  text-center mr-2 mb-2"><Link to="../contact">Lets talk!</Link></button>
-            <button type="button" class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-3 py-2.5 text-center mr-2 mb-2"><Link to="./about">About me</Link></button>
+            <button type="button" class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-3 py-2.5 text-center mr-2 mb-2"><a href="https://michaelpitop.tiiny.site/">My CV</a></button>
             {/* <button className='btn btn-primary' onClick={navigateToContacts}>Lets Talk</button> */}
 
           </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,9 @@ import "../styles.css";
 function Navbar() {
   return (
     <nav>
-      <div class="pr-6 flex gap-3 justify-end text-white font-bold">
+      <div class="flex grid-rows-4	 gap-3 justify-end  text-white font-bold">
           <div class="hover:scale-110"><Link to="/">Home</Link></div>
-          <div class="hover:scale-110"><Link to="./about">About</Link></div>
+          <div class="hover:scale-110"><Link to="./about">About me</Link></div>
           <div class="hover:scale-110"><Link to="./project">Projects</Link></div>
           <div class="hover:scale-110"><Link to="./contact">Contact</Link></div>
       </div>
