8d98f84931e1b760d4006f24a5b374faa644842f
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2023-06-27 15:20:08 +0200

    corrected some typos

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,11 +14,11 @@ function About() {
           <br />
           <p>Originally from the island of Crete, Greece and currently residing in the city of Munich, Germany. I'm an aspiring web developer searching for my first job in the field.</p>
           <br />
-          <p> I've always been interested in programming, I find it fascinating that you can bring your ideas to life and solve complex problems using logical and creative thinking. For this reason in 2021 I decided to make a leap of faith and try .</p>
+          <p> I've always been interested in programming, I find it fascinating that you can bring your ideas to life and solve complex problems using logical and creative thinking. For this reason in 2021 I decided to make a leap of faith and try to change my career path to web development.</p>
           <br />
-          <p>My journey into the world of web development began with my admission to Le Wagon Munich, a coding bootcamp that equipped me with the essential fundamentals of web development. During my time there, I gained valuable experience in collaborative teamwork, from the initial stages of development to production, including bug handling within a limited time frame.</p>
+          <p>My journey began with my admission to Le Wagon Munich, a coding bootcamp that equipped me with the essential fundamentals of web development. During my time there, I gained valuable experience in collaborative teamwork, from the initial stages of development to production, including bug handling within a limited time frame.</p>
           <br />
-          <p>After completing the bootcamp, I took it upon myself to continue expanding my knowledge and skills in web development by independently learning new tools such as React, TailwindCSS, C# and .NET tools, PHP and so much more. Despite maintaining a 60-hour work schedule, I spent most of my limited free time creating different small projects to gain practical skills and knowledge so that I could become job-ready but mostly because I'm eager to learn more.</p>
+          <p>After completing the bootcamp, I took it upon myself to continue expanding my knowledge and skills in web development by independently learning new tools such as React, TailwindCSS, C# and .NET tools, PHP and so much more. Despite maintaining a 60-hour work schedule, I spend most of my limited free time creating different small projects to gain practical skills and knowledge so that I could become job-ready but mostly because I'm eager to learn more.</p>
           <br />
           <p> I would love to work in challenging projects that provide a collaborative environment for teamwork and foster innovative solutions in web development.  </p>
         </div>
